8a39ddb5d41ea008f464dbd37dba500b8e6df5dd
Yifei Sun <ysun@hey.com>
2024-06-04 19:38:12 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    # nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # c
+    compat.url = "github:edolstra/flake-compat";
+    compat.flake = false;
+    # h
+    hm.url = "github:nix-community/home-manager";
+    hm.inputs.nixpkgs.follows = "nixpkgs";
+    # p
+    parts.url = "github:hercules-ci/flake-parts";
+    parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    # s
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { self, ... } @ inputs:
+    let inherit (self) outputs; in
+    inputs.parts.lib.mkFlake
+      {
+        inherit inputs;
+        specialArgs = { inherit outputs; };
+      }
+      {
+        systems = import inputs.systems;
+        imports = [
+          ./parts/formatter.nix
+        ];
+      };
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Ugawa Lab Nix/NixOS Infra
+
+[![Built with Nix](https://builtwithnix.org/badge.svg)](https://builtwithnix.org)
+
+## License
+
+All contents inside this repository, excluding submodules, are licensed under the [MIT License](license.txt).
+Third-party file(s) and/or code(s) are subject to their original term(s) and/or license(s).
