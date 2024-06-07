0530cd17456ffa85d65814be4048cc0c87eae6c3
Yifei Sun <ysun@hey.com>
2024-06-07 17:03:22 +0900

    add flake inputs

--- flake.nix
+++ flake.nix
@@ -1,27 +1,4 @@
 {
-  inputs = {
-    # nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # c
-    compat.url = "github:edolstra/flake-compat";
-    compat.flake = false;
-    # h
-    hm.url = "github:nix-community/home-manager";
-    hm.inputs.nixpkgs.follows = "nixpkgs";
-    # n
-    noshell.url = "github:viperml/noshell";
-    noshell.inputs.nixpkgs.follows = "nixpkgs";
-    # p
-    parts.url = "github:hercules-ci/flake-parts";
-    parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    # s
-    systems.url = "github:nix-systems/default";
-    # u
-    utils.url = "github:numtide/flake-utils";
-    utils.inputs.systems.follows = "systems";
-  };
-
   outputs = { self, ... } @ inputs:
     let inherit (self) outputs; in
     inputs.parts.lib.mkFlake
@@ -33,4 +10,59 @@
         systems = import inputs.systems;
         imports = [ ./parts ];
       };
+
+  inputs = {
+    # nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # c
+    compat.url = "github:edolstra/flake-compat";
+    compat.flake = false;
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    # d
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    # g
+    gitignore.url = "github:hercules-ci/gitignore.nix";
+    gitignore.inputs.nixpkgs.follows = "nixpkgs";
+    # h
+    hm.url = "github:nix-community/home-manager";
+    hm.inputs.nixpkgs.follows = "nixpkgs";
+    # i
+    impermanence.url = "github:nix-community/impermanence";
+    index.url = "github:nix-community/nix-index-database";
+    index.inputs.nixpkgs.follows = "nixpkgs";
+    # l
+    lanzaboote.url = "github:nix-community/lanzaboote/v0.3.0";
+    lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
+    lanzaboote.inputs.crane.follows = "crane";
+    lanzaboote.inputs.flake-compat.follows = "compat";
+    lanzaboote.inputs.flake-parts.follows = "parts";
+    lanzaboote.inputs.flake-utils.follows = "utils";
+    lanzaboote.inputs.pre-commit-hooks-nix.follows = "pre-commit-hooks";
+    lanzaboote.inputs.rust-overlay.follows = "rust-overlay";
+    # n
+    noshell.url = "github:viperml/noshell";
+    noshell.inputs.nixpkgs.follows = "nixpkgs";
+    # p
+    parts.url = "github:hercules-ci/flake-parts";
+    parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.inputs.nixpkgs-stable.follows = "nixpkgs";
+    pre-commit-hooks.inputs.flake-compat.follows = "compat";
+    pre-commit-hooks.inputs.gitignore.follows = "gitignore";
+    # r
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "utils";
+    # s
+    srvos.url = "github:nix-community/srvos";
+    srvos.inputs.nixpkgs.follows = "nixpkgs";
+    systems.url = "github:nix-systems/default";
+    # u
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.systems.follows = "systems";
+  };
 }
