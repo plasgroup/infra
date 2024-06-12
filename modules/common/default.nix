888e460efd6b0dde47962c143e44137ba07d9f76
Yifei Sun <ysun@hey.com>
2024-06-12 14:47:02 +0900

    clean up

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -3,6 +3,9 @@
 {
   imports = [
     inputs.index.nixosModules.nix-index
+    inputs.srvos.nixosModules.server
+    inputs.srvos.nixosModules.mixins-nix-experimental
+    inputs.srvos.nixosModules.mixins-trusted-nix-caches
 
     ./i18n.nix
     ./nix.nix
