071f6f611ff829595c2e4877570454954090e732
Yifei Sun <ysun@hey.com>
2024-06-12 15:08:39 +0900

    fix shells

--- modules/packages/shells.nix
+++ modules/packages/shells.nix
@@ -1,10 +1,12 @@
+{ unstable, ... }:
+
 {
   # noshell is enabled in `modules/users/default.nix`
   # docs: https://github.com/viperml/noshell
-  programs = {
-    bash.enable = true;
-    fish.enable = true;
-    nushell.enable = true;
-    zsh.enable = true;
-  };
+  environment.systemPackages = with unstable; [
+    bashInteractive
+    fish
+    nushell
+    zsh
+  ];
 }
