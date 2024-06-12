0cae120207d69104b45d636969879c277122d636
Yifei Sun <ysun@hey.com>
2024-06-12 14:10:12 +0900

    use ja_JP.UTF-8

---
+++ modules/common/i18n.nix
@@ -0,0 +1,19 @@
+{
+  i18n = {
+    defaultLocale = "ja_JP.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "ja_JP.UTF-8";
+      LC_IDENTIFICATION = "ja_JP.UTF-8";
+      LC_MEASUREMENT = "ja_JP.UTF-8";
+      LC_MONETARY = "ja_JP.UTF-8";
+      LC_NAME = "ja_JP.UTF-8";
+      LC_NUMERIC = "ja_JP.UTF-8";
+      LC_PAPER = "ja_JP.UTF-8";
+      LC_TELEPHONE = "ja_JP.UTF-8";
+      LC_TIME = "ja_JP.UTF-8";
+    };
+  };
+
+  # make sure less supports utf-8 character
+  environment.sessionVariables.LESSCHARSET = "utf-8";
+}
