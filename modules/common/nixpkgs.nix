7db34aaf69dcc0991d0bb8b5aeddba404fb49be1
Yifei Sun <ysun@hey.com>
2024-06-12 15:23:33 +0900

    nixpkgs instances config managed by ./lib/nixpkgs-config.nix

--- modules/common/nixpkgs.nix
+++ modules/common/nixpkgs.nix
@@ -1,6 +1,10 @@
 {
-  nixpkgs.config = {
-    allowUnfree = true;
-    allowUnfreePredicate = (_: true);
-  };
+  # eval error:
+  # "Your system configures nixpkgs with an externally created instance."
+  # "`nixpkgs.config` options should be passed when creating the instance instead."
+  # `pkgs`, and other nixpkgs instances' config managed by `./lib/nixpkgs-config.nix`
+  # nixpkgs.config = {
+  #   allowUnfree = true;
+  #   allowUnfreePredicate = (_: true);
+  # };
 }
