cc03d56536069a57f27d7e12679553c06a9c340b
Yifei Sun <ysun@hey.com>
2024-06-20 12:40:36 +0900

    add secureboot module

--- modules/secureboot/default.nix
+++ modules/secureboot/default.nix
@@ -1,5 +1,17 @@
-{ lib, inputs, ... }:
+{ lib, unstable, inputs, ... }:
 
 {
   imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
+
+  config = {
+    environment.systemPackages = [ unstable.sbctl ];
+
+    boot.loader.systemd-boot.enable = lib.mkForce false;
+
+    # docs: https://github.com/nix-community/lanzaboote
+    boot.lanzaboote = {
+      enable = true;
+      pkiBundle = "/etc/secureboot";
+    };
+  };
 }
