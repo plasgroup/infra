3d4f2764caf28a340db357c82beff624fb6f52cc
Yifei Sun <ysun@hey.com>
2024-06-23 19:56:14 +0900

    secureboot debug

--- modules/secureboot/default.nix
+++ modules/secureboot/default.nix
@@ -1,14 +1,16 @@
-{ lib, unstable, inputs, ... }:
+{ lib, pkgs, inputs, ... }:
 
 {
   imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
 
   config = {
-    environment.systemPackages = [ unstable.sbctl ];
+    environment.systemPackages = [ pkgs.sbctl ];
 
+    boot.loader.grub.enable = lib.mkForce false;
     boot.loader.systemd-boot.enable = lib.mkForce false;
 
     # docs: https://github.com/nix-community/lanzaboote
+    boot.bootspec.enable = true;
     boot.lanzaboote = {
       enable = true;
       pkiBundle = "/etc/secureboot";
