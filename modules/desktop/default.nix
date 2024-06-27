ebeea96448b6d9edb8ba435fe945737a7d606b7d
Yifei Sun <ysun@hey.com>
2024-06-27 21:43:01 +0900

    add fonts

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -1,8 +1,32 @@
+{ lib, pkgs, ... }:
+
 {
   # currently only supports gnome, we might add KDE or other DE support in the future
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
+  };
+
+  fonts = {
+    fontDir.enable = true;
+    enableDefaultPackages = true;
+    enableGhostscriptFonts = true;
+
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+    ];
+
+    fontconfig = {
+      enable = true;
+      allowBitmaps = true;
+      antialias = true;
+      hinting = {
+        enable = true;
+        style = "full";
+      };
+    };
   };
 }
