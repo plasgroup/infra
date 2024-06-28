f5ae48493a051943eb32d86b6df06a748892115e
Yifei Sun <ysun@hey.com>
2024-06-28 16:05:13 +0900

    move nixos modules under modules/nixos

--- parts/nixos-modules.nix
+++ parts/nixos-modules.nix
@@ -6,11 +6,11 @@ in
 {
   flake.nixosModules =
     lib.genAttrs
-      (lib.filesIn ../modules)
+      (lib.filesIn ../modules/nixos)
       (module: moduleWithSystem (
         # must use explicit args here to pass them to imported nixos module
         # see `moduleWithSystem` in flake-parts docs for more details
         { inputs, outputs, lib, pkgs, unstable }:
-        { imports = [ (import (../modules/. + "/${module}")) ]; }
+        { imports = [ (import (../modules/nixos/. + "/${module}")) ]; }
       ));
 }
