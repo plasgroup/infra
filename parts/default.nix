f7976f92cd4e1b58e6abce757eb1519fd7d9b079
Yifei Sun <ysun@hey.com>
2024-06-28 16:42:34 +0900

    auto import flake parts

--- parts/default.nix
+++ parts/default.nix
@@ -2,27 +2,34 @@ args:
 
 let
   inherit (args) inputs outputs;
-  inherit (outputs) lib;
-  config = lib.nixpkgsConfig;
 in
 {
-  imports = [
-    ./dev-shells.nix
-    ./formatter.nix
-    ./lib.nix
-    ./nixos-configurations.nix
-    ./nixos-modules.nix
-  ];
+  # automatic import, but must exclude `default.nix` and `lib.nix` to prevent infinite recursion
+  imports = [ ./lib.nix ] ++ (
+    let
+      inherit (inputs.nixpkgs) lib;
+    in
+    # the `lib` here is from nixpkgs
+    builtins.map (f: ./. + "/${f}") (lib.filter
+      (f: !(f == "default.nix" || f == "lib.nix"))
+      (lib.attrNames (builtins.readDir ./.)))
+  );
 
-  perSystem = { system, ... }: {
-    # args managed by `./lib/mk-module-args.nix`
-    _module.args = lib.mkModuleArgs {
-      inherit system config;
-      instances = [
-        { instance = inputs.nixpkgs; name = "pkgs"; }
-        { instance = inputs.unstable; name = "unstable"; }
-      ];
-      extraArgs = { inherit inputs outputs lib; };
+  perSystem = { system, ... }:
+    let
+      # the `lib` here is exported `./lib`
+      inherit (outputs) lib;
+      config = lib.nixpkgsConfig;
+    in
+    {
+      # args managed by `./lib/mk-module-args.nix`
+      _module.args = lib.mkModuleArgs {
+        inherit system config;
+        instances = [
+          { instance = inputs.nixpkgs; name = "pkgs"; }
+          { instance = inputs.unstable; name = "unstable"; }
+        ];
+        extraArgs = { inherit inputs outputs lib; };
+      };
     };
-  };
 }
